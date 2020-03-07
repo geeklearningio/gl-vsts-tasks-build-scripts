ceb983fe10fed2d453d6fd6ef982e9c8b5b11ccf
Cyprien Autexier <2660262+sandorfr@users.noreply.github.com>
2020-03-07 13:07:10 +1100

    bump version, migrate to eslint delete dist

--- dist/configuration.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var path = require("path");
-function getConfiguration() {
-    var currentDirectory = process.cwd();
-    return require(path.join(currentDirectory, "configuration.json"));
-}
-exports.getConfiguration = getConfiguration;
